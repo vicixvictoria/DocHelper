f9c75bda9279f767acfdee1b3fbb2bdcf4de935d
Manuel <e0671428@student.tuwien.ac.at>
2022-04-12 10:40:35 +0200

    Commits backend

---
+++ backend/src/test/java/at/ac/tuwien/dochelper/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.dochelper.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

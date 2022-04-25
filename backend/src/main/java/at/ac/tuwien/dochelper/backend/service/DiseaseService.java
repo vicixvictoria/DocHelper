8d3d11fddcadc7b94ed992ba5807f888370ca39e
vicizeillinger <e11809914@student.tuwien.ac.at>
2022-04-25 23:57:25 +0200

    User-Story-2: added Validation for Disease and merged Patient User Story in (for preventing merge conflicts)

--- backend/src/main/java/at/ac/tuwien/dochelper/backend/service/DiseaseService.java
+++ backend/src/main/java/at/ac/tuwien/dochelper/backend/service/DiseaseService.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.dochelper.backend.service;
 
 import at.ac.tuwien.dochelper.backend.entity.Disease;
 import at.ac.tuwien.dochelper.backend.repository.DiseaseRepository;
+import at.ac.tuwien.dochelper.backend.validator.DiseaseValidator;
+import at.ac.tuwien.dochelper.backend.validator.PatientValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +14,12 @@ import java.util.List;
 public class DiseaseService {
 
     private final DiseaseRepository diseaseRepository;
+    private final DiseaseValidator diseaseValidator;
 
     @Autowired
-    public DiseaseService(DiseaseRepository diseaseRepository) {
+    public DiseaseService(DiseaseRepository diseaseRepository, DiseaseValidator diseaseValidator) {
         this.diseaseRepository = diseaseRepository;
+        this.diseaseValidator = diseaseValidator;
 
     }
 
@@ -26,13 +30,14 @@ public class DiseaseService {
     @Transactional
     public Disease createDisease(Disease disease)
     {
-        //diseaseValidator.validateNewDisease(disease);
+        diseaseValidator.validateNewDisease(disease);
         return diseaseRepository.save(disease);
     }
 
+    /*
     @Transactional
     public Disease updateDisease(Disease disease) {
-        //diseaseValidator.validateUpdateDisease(disease);
+        diseaseValidator.validateUpdateDisease(disease);
         return diseaseRepository.save(disease);
-    }
+    }*/
 }

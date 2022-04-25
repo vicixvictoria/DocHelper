ae5d04f75182b6ac4e4de8446e8cb8072f28fd85
Benjamin Redl <benjamin.redl.99@gmail.com>
2022-04-25 23:12:35 +0200

    fixed Sex backend

--- backend/src/main/java/at/ac/tuwien/dochelper/backend/util/Sex.java
+++ backend/src/main/java/at/ac/tuwien/dochelper/backend/util/Sex.java
@@ -1,25 +1,5 @@
 package at.ac.tuwien.dochelper.backend.util;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Sex {
-    @JsonProperty("FEMALE")
-    FEMALE("female"),
-    @JsonProperty("MALE")
-    MALE("male"),
-    OTHER("other");
-
-    private final String sex_value;
-
-    private Sex(String sex_value) {
-        this.sex_value = sex_value;
-    }
-
-    @JsonValue
-    public String getSex_value() {
-        return sex_value;
-    }
+    MALE, FEMALE;
 }

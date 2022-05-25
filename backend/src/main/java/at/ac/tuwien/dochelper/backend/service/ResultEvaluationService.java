cf0695ea47e241f8f0b7219e0505961510df81eb
Manuel <e0671428@student.tuwien.ac.at>
2022-05-25 17:43:52 +0200

    #14 adds further implementation of evaluation algorithm

--- backend/src/main/java/at/ac/tuwien/dochelper/backend/service/ResultEvaluationService.java
+++ backend/src/main/java/at/ac/tuwien/dochelper/backend/service/ResultEvaluationService.java
@@ -1,8 +1,14 @@
 package at.ac.tuwien.dochelper.backend.service;
 
+import at.ac.tuwien.dochelper.backend.entity.Disease;
 import at.ac.tuwien.dochelper.backend.entity.DiseaseScore;
+import at.ac.tuwien.dochelper.backend.entity.LabValue;
 import at.ac.tuwien.dochelper.backend.entity.TestResult;
+import at.ac.tuwien.dochelper.backend.entity.TestValueThreshold;
 import at.ac.tuwien.dochelper.backend.exception.ValidationException;
+import at.ac.tuwien.dochelper.backend.repository.DiseaseRepository;
+import at.ac.tuwien.dochelper.backend.repository.LabValueRepository;
+import at.ac.tuwien.dochelper.backend.repository.TestValueThresholdRepository;
 import at.ac.tuwien.dochelper.backend.util.EvaluationAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,17 +19,30 @@ import java.util.List;
 public class ResultEvaluationService {
 
     private final EvaluationAlgorithm evaluationAlgorithm;
+    private final DiseaseRepository diseaseRepository;
+    private final TestValueThresholdRepository testValueThresholdRepository;
+    private final LabValueRepository labValueRepository;
 
     @Autowired
-    public ResultEvaluationService(EvaluationAlgorithm evaluationAlgorithm) {
+    public ResultEvaluationService(EvaluationAlgorithm evaluationAlgorithm, DiseaseRepository diseaseRepository, TestValueThresholdRepository testValueThresholdRepository, LabValueRepository labValueRepository) {
         this.evaluationAlgorithm = evaluationAlgorithm;
+        this.diseaseRepository = diseaseRepository;
+        this.testValueThresholdRepository = testValueThresholdRepository;
+        this.labValueRepository = labValueRepository;
     }
+
+
+
 
     public List<DiseaseScore> evaluateTestResult(TestResult testResult) {
         if (testResult.getLabMeasures() == null) {
             throw new ValidationException("test result must contain measures to be analyzed");
         }
-        return evaluationAlgorithm.evaluateResult(testResult);
+        List<Disease> diseaseList = diseaseRepository.findAll();
+        List<LabValue> labValueList = labValueRepository.findAll();
+        List<TestValueThreshold> testValueThresholdList = testValueThresholdRepository.findAll();
+
+        return evaluationAlgorithm.evaluateResult(testResult, labValueList, diseaseList, testValueThresholdList);
     }
 
 }
--- backend/src/main/java/at/ac/tuwien/dochelper/backend/util/EvaluationAlgorithm.java
+++ backend/src/main/java/at/ac/tuwien/dochelper/backend/util/EvaluationAlgorithm.java
@@ -1,19 +1,48 @@
 package at.ac.tuwien.dochelper.backend.util;
 
+import at.ac.tuwien.dochelper.backend.entity.Disease;
 import at.ac.tuwien.dochelper.backend.entity.DiseaseScore;
+import at.ac.tuwien.dochelper.backend.entity.LabMeasure;
+import at.ac.tuwien.dochelper.backend.entity.LabValue;
 import at.ac.tuwien.dochelper.backend.entity.TestResult;
+import at.ac.tuwien.dochelper.backend.entity.TestValueThreshold;
+import at.ac.tuwien.dochelper.backend.exception.ValidationException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class EvaluationAlgorithm {
 
-    public List<DiseaseScore> evaluateResult(TestResult testResult) {
+    public List<DiseaseScore> evaluateResult(TestResult testResult, List<LabValue> labValueList, List<Disease> diseaseList, List<TestValueThreshold> testValueThresholdList) {
 
         List<DiseaseScore> result = new ArrayList<>();
-        result.add(new DiseaseScore(null, 2.3));
+        for (Disease disease : diseaseList) {
+            result.add(new DiseaseScore(disease, null));
+        }
+
+        for (Disease disease : diseaseList) {
+
+            List<TestValueThreshold> potentialThresholds = testValueThresholdList.stream()
+                    .filter(th -> th.getDiseaseName().equals(disease.getDiseaseName())).collect(Collectors.toList());
+
+            for (LabMeasure labMeasure: testResult.getLabMeasures()) {
+
+                List<TestValueThreshold> testValueThreshold = potentialThresholds.stream()
+                        .filter(th -> th.getLabVal().equals(labMeasure.getLabValue())).collect(Collectors.toList());
+
+                if (testValueThreshold.size() != 1) {
+                 throw new ValidationException("For disease: " + disease.getDiseaseName() + "and lab value: " + labMeasure.getLabValue().getLabValName()
+                         + "more than one threshold exists");
+                }
+
+                //further evaluation
+            }
+        }
+
+
         return result;
     }
 }

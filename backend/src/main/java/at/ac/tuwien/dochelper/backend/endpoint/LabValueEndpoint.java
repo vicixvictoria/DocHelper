1ecdedd6a48e95f3824da66f85079f97718b6b64
Benjamin Redl <benjamin.redl.99@gmail.com>
2022-06-02 15:37:30 +0200

    backend: added deleteLabValue and addLabValue

--- backend/src/main/java/at/ac/tuwien/dochelper/backend/endpoint/LabValueEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/dochelper/backend/endpoint/LabValueEndpoint.java
@@ -1,14 +1,18 @@
 package at.ac.tuwien.dochelper.backend.endpoint;
 
 import at.ac.tuwien.dochelper.backend.endpoint.dto.LabValueDto;
+import at.ac.tuwien.dochelper.backend.endpoint.dto.PatientDto;
 import at.ac.tuwien.dochelper.backend.endpoint.mapper.LabValueMapper;
 import at.ac.tuwien.dochelper.backend.entity.LabValue;
+import at.ac.tuwien.dochelper.backend.entity.Patient;
 import at.ac.tuwien.dochelper.backend.service.LabValueService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
 
@@ -30,6 +34,21 @@ public class LabValueEndpoint {
     public List<LabValueDto> getAllLabVals() {
         LOGGER.info("GET api/v1/labValues ");
         return labValueMapper.labValueToLabValueDto(labValueService.getAllLabVals());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LabValueDto createLabValue(@Valid @RequestBody LabValueDto labValueDto) {
+        LOGGER.info("POST /api/v1/labValues: {} ", labValueDto.toString());
+        LabValue labValue = labValueMapper.labValueDtoToLabValue(labValueDto);
+        labValue = labValueService.createLabVal(labValue);
+        return labValueMapper.labValueToLabValueDto(labValue);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{labValId}")
+    public void deleteLabValue(@PathVariable Long labValId) {
+        labValueService.deleteLabValue(labValId);
     }
 
     /*
